ba0424183736bbea7e04f2a43d4f425f61a62fd9
SatyaNurhutama <satyanurhutama1926@gmail.com>
2022-04-13 12:48:02 +0000

    Add field category_id in food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,7 @@
 class Food < ApplicationRecord
+
+  has_one :categories
+  
   validates :name, presence: true, uniqueness: true, length: { minimum: 2 }
   validates :price,  numericality: { greater_than_or_equal_to: 0.01 }, format: { with: /\A\d+\z/, message: "Integer only. No sign allowed." }
 
--- db/migrate/20220407015323_create_foods.rb
+++ db/migrate/20220407015323_create_foods.rb
@@ -4,6 +4,7 @@ class CreateFoods < ActiveRecord::Migration[7.0]
       t.string :name
       t.text :description
       t.integer :price
+      t.string :category_id
 
       t.timestamps
     end
